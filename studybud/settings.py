829fa73c4c4feced136cf6b2b43c2f85489110b1
hephhay <faradaydanfard@gmail.com>
2022-07-09 01:00:17 +0100

    added allowed hosts

--- studybud/settings.py
+++ studybud/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://studybud-social.herokuapp.com"]
+ALLOWED_HOSTS = ['studybud-social.herokuapp.com']
 
 
 # Application definition
